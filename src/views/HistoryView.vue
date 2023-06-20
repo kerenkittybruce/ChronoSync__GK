e9da8f27ddff38fd1ac31f07a958867c86266483
Keren Bruce <kerenchelseabruce@gmail.com>
2023-06-20 10:04:10 +0200

    Edited HistoryView & Removed HistoryModal Component

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -1,11 +1,109 @@
 <template>
+
+    <!-- If user is an 'admin', show 'history modal' -->
+
     <div v-if="user.role === 'admin'">
         <h1>User History</h1>
         <br>
         <h3>Search for a name and their timesheet will appear below :</h3>
 
-        <h4 class="text-center">The timesheet will appear here</h4>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Login History
+        </button>
+
+        <!-- Bootstrap Modal By Abdul Abrahams -->
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">User Login History</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div>
+                            <a class="btn btn-primary dropdown-toggle" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            June 2023
+                            </a>
+                            <div class="collapse" id="collapseExample">
+                                <div class="card card-body">
+                                    1 June 2023
+                                    <div class="row">
+                                        <input class="col-6" value="8: 23 am">
+                                        <input class="col-6" value="16:30 pm">
+                                    </div>
+                                    2 June 2023
+                                    <div class="row">
+                                        <input class="col-6" value="8: 23 am">
+                                        <input class="col-6" value="16:30 pm">
+                                    </div>
+                                    3 June 2023
+                                    <div class="row">
+                                        <input class="col-6" value="8: 23 am">
+                                        <input class="col-6" value="16:30 pm">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <a class="btn btn-primary dropdown-toggle" data-bs-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            July 2023
+                        </a>
+                        <div class="collapse" id="collapseExample2">
+                            <div class="card card-body">
+                                1 July 2023
+                                <div class="row">
+                                    <input class="col-6" value="8: 23 am">
+                                    <input class="col-6" value="16:30 pm">
+                                </div>
+                                2 July 2023
+                                <div class="row">
+                                    <input class="col-6" value="8: 23 am">
+                                    <input class="col-6" value="16:30 pm">
+                                </div>
+                                3 July 2023
+                                <div class="row">
+                                    <input class="col-6" value="8: 23 am">
+                                    <input class="col-6" value="16:30 pm">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <a class="btn btn-primary dropdown-toggle" data-bs-toggle="collapse" href="#collapseExample3" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            August 2023
+                        </a>
+                        <div class="collapse" id="collapseExample3">
+                            <div class="card card-body">
+                                1 August 2023
+                                <div class="row">
+                                    <input class="col-6" value="8: 23 am">
+                                    <input class="col-6" value="16:30 pm">
+                                </div>
+                                2 August 2023
+                                <div class="row">
+                                    <input class="col-6" value="8: 23 am">
+                                    <input class="col-6" value="16:30 pm">
+                                </div>
+                                3 August 2023
+                                <div class="row">
+                                    <input class="col-6" value="8: 23 am">
+                                    <input class="col-6" value="16:30 pm">
+                                </div>
+                            </div>
+                       </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
     </div>
+
+    <!-- If user is a 'user', show 'error message' -->
 
     <div class="no-admin container" v-else>
         <div class="row vh-100 d-flex justify-content-center align-content-center">
